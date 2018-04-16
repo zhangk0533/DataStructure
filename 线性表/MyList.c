d7190ebc696c4b956e6e625ea71625a3039555f3
zhangk0533 <907144187@qq.com>
2018-04-16 23:27:12 +0800

    123

---
+++ 线性表/MyList.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+#define OVERFLOW -2
+
+#define LIST_INIT_SIZE 100  //线性表存储空间的初始分配量 
+#define LISTINCREMENT 10  //线性表存储空间的分配增量 
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+typedef int Status;
+
+typedef struct{
+	int *elem;  //存储空间基址 
+	int length;  //当前长度 
+	int listsize;  //当前分配的存储容量(以sizeof(int)为单位) 
+}MyList;
+
+Status InitList(MyList *L);
+Status ListInsert(MyList *L,int i,int e);
+Status ListDelete(MyList *L,int i,int *e);
+
+int main(int argc, char *argv[]) {
+	return 0;
+}
+
+
+
+
