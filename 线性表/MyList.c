f0e1ac0e34c72826d7c8894e399e722376f50dee
zhangk0533 <907144187@qq.com>
2018-04-17 16:33:46 +0800

    123

--- 线性表/MyList.c
+++ 线性表/MyList.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
 
 #define TRUE 1
 #define FALSE 0
@@ -26,9 +27,51 @@ Status ListInsert(MyList *L,int i,int e);
 Status ListDelete(MyList *L,int i,int *e);
 
 int main(int argc, char *argv[]) {
+	MyList L;
+	InitList(&L);
+	int i=0;
+	ListInsert(&L,0,1);
+	ListInsert(&L,1,2);
+	printf("%d",*(L.elem+1));
 	return 0;
 }
 
+Status InitList(MyList *L)
+{
+	L->elem = (int *)(malloc(sizeof(int)*LIST_INIT_SIZE));
+	if(L->elem==NULL) return ERROR;
+	L->length = 0;
+	L->listsize = LIST_INIT_SIZE;
+}
 
+Status ListInsert(MyList *L,int i,int e)
+{
+	if(i<0||i>L->length+1) return ERROR;
+	if(L->length==LIST_INIT_SIZE)
+	{
+		int *newbase = (int *)(realloc(L->elem,sizeof(int)*(LIST_INIT_SIZE+LISTINCREMENT)));
+		L->elem = newbase;
+	}
+	int *q;
+	int *p = L->elem+i;
+	for(q=L->elem+L->length;q>=p;q--)
+	{
+		*(q+1) = *q;
+	}
+	*p = e;
+	return OK;
+}
+
+Status ListDelete(MyList *L,int i,int *e)
+{
+	if(i<0||i>L->length+1) return ERROR;
+	int *q = L->elem+i;
+	*e = *q;
+	for(++q;q<L->elem+L->length;q++)
+	{
+		*q = *(q+1);
+	}
+	return OK;
+}
 
 
