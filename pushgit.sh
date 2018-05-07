8c9a8e76db0d0c04327908507ad1b1345bde97db
zhangk0533 <907144187@qq.com>
2018-05-07 18:20:53 +0800

    0507

--- pushgit.sh
+++ pushgit.sh
@@ -5,7 +5,6 @@
 # Created Time: Sat 28 Apr 2018 12:13:39 AM PDT
 #########################################################################
 #!/bin/bash
-
 git add .
 git commit -m $1
-git push origin master
+git push 
--- 栈/MyStack_maze.c
+++ 栈/MyStack_maze.c
@@ -11,15 +11,24 @@
 #define STACK_INIT_SIZE 100  //存储空间初始分配量
 #define STACKINCREMENT 10  //存储空间分配增量 
 
-#define MazeScale 2
+#define MazeScale 10
 
 typedef int Status;
 
 typedef int MazeType[MazeScale][MazeScale];
 
-MazeType maze;
-
-int a[2][2] = {{2,2},{2,2}};
+MazeType maze = {
+	{2,2,2,2,2,2,2,2,2,2},
+	{2,0,0,2,0,0,0,2,0,2},
+	{2,0,0,2,0,0,0,2,0,2},
+	{2,0,0,0,0,2,2,0,0,2},
+	{2,0,2,2,2,0,0,0,0,2},
+	{2,0,0,0,2,0,0,0,0,2},
+	{2,0,2,0,0,0,2,0,0,2},
+	{2,0,2,2,2,0,2,2,0,2},
+	{2,2,0,0,0,0,0,0,0,2},
+	{2,2,2,2,2,2,2,2,2,2}
+};
 
 typedef struct{
 	int x;
@@ -56,6 +65,48 @@ Status MarkPrint(PosType pos);
 
 int main(int argc, char *argv[]) {
 	MyStack S;
+    int i,j;
+	for(i=0;i<MazeScale;i++)
+	{
+		for(j=0;j<MazeScale;j++)
+		{
+			if(maze[i][j]==2)
+			{
+				printf("#");
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+	PosType start;
+	PosType end;
+	start.x = 1;
+	start.y = 1;
+	end.x = 8;
+	end.y = 8;
+	MazePath(maze,start,end);
+	for(i=0;i<MazeScale;i++)
+	{
+		for(j=0;j<MazeScale;j++)
+		{
+			if(maze[i][j]==2)
+			{
+				printf("#");
+			}
+			else if(maze[i][j]==1)
+			{
+				printf(".");
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
 	return 0;
 }
 
@@ -139,8 +190,49 @@ Status Pop(MyStack *S,SElemType *e)
 	return OK;
 }
 
-Status MazePath(MazeType maze,PosType Start,PosType end)
+Status MazePath(MazeType maze,PosType start,PosType end)
 {
+	MyStack S;
+	InitStack(&S);
+	int cusStep = 1;
+	PosType curPos = start;
+	do
+	{
+		if(Pass(curPos))
+		{
+			FootPrint(curPos);
+			SElemType e;
+			e.di = 1;
+			e.ord = cusStep;
+			e.seat = curPos;
+			Push(&S,e);
+			if(e.seat.x==end.x&&e.seat.y==end.y)
+			{
+				break;
+			}
+			curPos = NextPos(curPos,e.di);
+			cusStep++;
+		}
+		else
+		{
+			cusStep--;
+			SElemType e;
+			Pop(&S,&e);
+			while(e.di==4&&!StackEmpty(S))
+			{
+				MarkPrint(e.seat);
+				Pop(&S,&e);
+				cusStep--;
+			}
+			if(e.di<4)
+			{
+				curPos = NextPos(e.seat,e.di+1);
+				e.di++;
+				cusStep++;
+				Push(&S,e);
+			}
+		}
+	}while(!StackEmpty(S));
 	return OK;
 }
 
@@ -168,19 +260,19 @@ PosType NextPos(PosType pos,int di)
 	switch(di)
 	{
 		case 1:
+			nextPos.x = pos.x;
+			nextPos.y = pos.y+1;
+			break;
+		case 2:
 			nextPos.x = pos.x+1;
 			nextPos.y = pos.y;
 			break;
-		case 2:
+		case 3:
 			nextPos.x = pos.x;
 			nextPos.y = pos.y-1;
-			break;
-		case 3:
+		case 4:
 			nextPos.x = pos.x-1;
 			nextPos.y = pos.y;
-		case 4:
-			nextPos.x = pos.x;
-			nextPos.y = pos.y+1;
 	}
 	return nextPos;
 }
